abca1c83f00fc82efd2de9b01d03ef0b4781cf89
Sahib Sawhney <47554616+WhiteHatter-WH@users.noreply.github.com>
2022-01-19 15:59:57 -0600

    first commit

---
+++ TravelAgency/src/Travel/Airline.java
@@ -0,0 +1,48 @@
+package Travel;
+
+import exception.BadParameterException;
+import exception.NullParameterException;
+
+import java.util.Objects;
+
+public class Airline {
+    private String airlineName;
+
+    public Airline(String airlineName) throws BadParameterException, NullParameterException {
+        setAirlineName(airlineName);
+    }
+
+    private void setAirlineName(String aName) throws NullParameterException, BadParameterException {
+        if (aName == null) {
+            throw new NullParameterException("Null value passed in for setAirlineName");
+        }
+        if (aName.length() > 8) {
+            throw new BadParameterException("Bad value passed to setAirlineName: " + aName);
+        }
+        airlineName = aName;
+    }
+
+    public String getAirlineName()  {
+        return airlineName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Airline airline = (Airline) o;
+        return airlineName.equals(airline.airlineName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(airlineName);
+    }
+
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "airlineName='" + airlineName + '\'' +
+                '}';
+    }
+}
